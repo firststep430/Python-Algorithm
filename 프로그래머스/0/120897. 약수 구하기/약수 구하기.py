776854a26af59a64365489ef4f06924aef309f49
firststep430 <gene22sis2011@gmail.com>
2025-01-28 22:23:53 +0900

    [level 0] Title: 약수 구하기, Time: 1.26 ms, Memory: 9.91 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    result = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            result.append(i)
+        else:
+            pass
+    
+    result.sort()
+    
+    return result
